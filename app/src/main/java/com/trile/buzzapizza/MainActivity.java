d03ef43eaca5b2f268cd3945da665c36991c1e42
Tri Le <trile95.it@gmail.com>
2022-04-15 22:28:35 -0400

    show greeting based on current time

--- app/src/main/java/com/trile/buzzapizza/MainActivity.java
+++ app/src/main/java/com/trile/buzzapizza/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewGreeting;
 
     ListView listViewHistoryOrders;
     List<HistoryOrderItem> items;
@@ -19,9 +24,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewGreeting = findViewById(R.id.text_view_greeting);
+
         listViewHistoryOrders = findViewById(R.id.list_view_history_orders);
 
+        setupTextViewGreeting();
         setupHistoryOrdersListView();
+    }
+
+    private void setupTextViewGreeting() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+        String[] dayTime = getResources().getStringArray(R.array.day_time);
+        String greeting = getResources().getString(R.string.greeting);
+
+        String dayTimeToShow;
+        if (hour >= 6) {
+            dayTimeToShow = String.format(greeting, dayTime[2]);
+        } else if (hour >= 12) {
+            dayTimeToShow = String.format(greeting, dayTime[1]);
+        } else if (hour >= 6) {
+            dayTimeToShow = String.format(greeting, dayTime[0]);
+        } else {
+            dayTimeToShow = String.format(greeting, dayTime[3]);
+        }
+        textViewGreeting.setText(dayTimeToShow);
     }
 
     private void setupHistoryOrdersListView() {
